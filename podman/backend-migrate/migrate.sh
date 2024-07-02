81aad3133fafdac9b78bab7e6bc920eb2e50a76f
Choon-Siang Lai <mycyberpet@yahoo.com>
2024-07-02 16:57:54 +0800

    now it should be able to fetch archive from hf properly (#5)

--- podman/backend-migrate/migrate.sh
+++ podman/backend-migrate/migrate.sh
@@ -13,8 +13,8 @@ poetry install --only=main
 # initiate login
 poetry run huggingface-cli login --token $HUGGINGFACE_TOKEN
 # download from sinar/legisdata
-#mkdir -p data
-#huggingface-cli download sinarproject/legisdata --repo-type dataset --local-dir ./data
+mkdir -p data
+poetry run huggingface-cli download sinarproject/legisdata --repo-type dataset --local-dir ./data
 
 poetry run python manage.py migrate
 poetry run python manage.py import-legisdata 2020 2
